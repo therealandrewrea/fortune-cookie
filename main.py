a75b753788f25f4c387d0822f11fd61cb389d488
Andy Rea <therealandrewrea@gmail.com>
2017-03-21 19:07:09 -0500

    added base content

--- main.py
+++ main.py
@@ -15,10 +15,34 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    fortune_list = ["The wise man doesn't listen to cookies",
+    "A Tiger with a fully belly is a friend",
+    "My boss is a jerk",
+    "Something fortuitous will befall you soonish",
+    "Look behind you"
+    ]
+    index = random.randint(0,4)
+    return fortune_list[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = "<strong>"+getRandomFortune()+"</strong>"
+        fortune_sen ="Your fortune: "+fortune
+        fortune_p = "<p>"+fortune_sen+"</p>"
+
+        lucky_number = "<strong>"+str(random.randint(1,100))+"</strong>"
+        lucky = 'Your Lucky Number is '+ lucky_number
+        lucky_p = "<p>"+lucky+"</p>"
+
+        another_cookie = "<button><a href='.'><strong><em>One more couldn't hurt...</strong></em></a></button>"
+
+        content = header + fortune_p + lucky_p + another_cookie
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
